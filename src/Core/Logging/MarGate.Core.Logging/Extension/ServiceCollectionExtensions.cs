426eb0599359be5afc9e1a6c984e56e477f7a8bd
Busra Tanriverdi <busra.tuglu@adesso.com.tr>
2024-11-14 21:02:53 +0300

    implemented logging into core

---
+++ src/Core/Logging/MarGate.Core.Logging/Extension/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace MarGate.Core.Logging.Extension;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddLog(this IServiceCollection services, IHostBuilder hostBuilder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        hostBuilder.UseSerilog(Log.Logger);
+
+        return services;
+    }
+}
