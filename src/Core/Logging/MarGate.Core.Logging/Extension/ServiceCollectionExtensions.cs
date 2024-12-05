94b3f8ecce33399e0ca2d10e79f8b080cdc5e3ab
Busra Tanriverdi <busra.tuglu@adesso.com.tr>
2024-12-05 20:55:12 +0300

    refactored logging

--- src/Core/Logging/MarGate.Core.Logging/Extension/ServiceCollectionExtensions.cs
+++ src/Core/Logging/MarGate.Core.Logging/Extension/ServiceCollectionExtensions.cs
@@ -1,19 +1,41 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System.ComponentModel;
 
 namespace MarGate.Core.Logging.Extension;
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddLog(this IServiceCollection services, IHostBuilder hostBuilder)
+    public static IServiceCollection AddLog(this IServiceCollection services, LoggingOptions options, IHostBuilder hostBuilder)
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .WriteTo.Console()
-            .CreateLogger();
+        var loggerConfiguration = new LoggerConfiguration().MinimumLevel.Information();
+        SetWriteToConfiguration(loggerConfiguration, options.WriteToType);
+        Log.Logger = loggerConfiguration.CreateLogger();
 
         hostBuilder.UseSerilog(Log.Logger);
 
         return services;
     }
+
+    private static void SetWriteToConfiguration(LoggerConfiguration loggerConfiguration, WriteToType writeToType)
+    {
+        switch (writeToType)
+        {
+            case WriteToType.Console:
+                loggerConfiguration.WriteTo.Console();
+                break;
+            default:
+                throw new InvalidEnumArgumentException(nameof(WriteToType));
+        }
+    }
+}
+
+public class LoggingOptions
+{
+    public WriteToType WriteToType { get; set; }
+}
+
+public enum WriteToType
+{
+    Console,
 }
