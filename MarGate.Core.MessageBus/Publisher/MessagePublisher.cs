b1123d8acd3ede474c1f3c6e2648744460143b0d
Busra Tanriverdi <busra.tuglu@adesso.com.tr>
2024-12-05 23:20:44 +0300

    implemented message bus into core

---
+++ MarGate.Core.MessageBus/Publisher/MessagePublisher.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+
+namespace MarGate.Core.MessageBus.Publisher;
+
+internal class MessagePublisher(IPublishEndpoint publishEndpoint) : IMessagePublisher
+{
+    private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
+
+    public Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken) where TMessage : class, IMessage
+    {
+        return _publishEndpoint.Publish(message, cancellationToken);
+    }
+}
