d0a31e29f42c8e9f6853bf269670b7fbbac4c166
Busra Tanriverdi <busra.tuglu@adesso.com.tr>
2024-12-21 00:16:22 +0300

    implemented entity confiurations for all entities

---
+++ src/Services/Order/Infrastructure/MarGate.Order.Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace MarGate.Order.Persistence.Configurations;
+
+public void Configure(EntityTypeBuilder<Order> builder)
+{
+    builder.HasKey(o => o.Id);
+    builder.Property(o => o.Id).ValueGeneratedOnAdd();
+    builder.Property(o => o.Address).IsRequired().HasMaxLength(250);
+    builder.Property(o => o.Description).IsRequired().HasMaxLength(150);
+
+    builder.Property(o => o.CreatedDate)
+           .IsRequired()
+           .HasDefaultValueSql("GETDATE()");
+
+    builder.Property(o => o.ModifiedDate)
+           .IsRequired()
+           .ValueGeneratedOnAddOrUpdate()
+           .HasDefaultValueSql("GETDATE()");
+
+    builder.Property(o => o.IsDeleted).IsRequired();
+
+    builder.HasOne<User>(o => o.User)
+           .WithMany(u => u.Orders)
+           .HasForeignKey(o => o.UserId);
+
+    builder.ToTable("Orders");
+}
---
+++ src/Services/Order/Infrastructure/MarGate.Order.Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using MarGate.Order.Domain.Entities;
+
+namespace MarGate.Order.Persistence.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(oi => oi.Id);
+        builder.Property(oi => oi.Id).ValueGeneratedOnAdd();
+
+        builder.Property(oi => oi.CreatedDate)
+               .IsRequired()
+               .HasDefaultValueSql("GETDATE()"); 
+
+        builder.Property(oi => oi.ModifiedDate)
+               .IsRequired()
+               .ValueGeneratedOnAddOrUpdate() 
+               .HasDefaultValueSql("GETDATE()"); 
+
+        builder.Property(oi => oi.IsDeleted).IsRequired();
+
+        builder.HasOne<Product>(oi => oi.Product)
+               .WithMany(p => p.OrderItems)
+               .HasForeignKey(oi => oi.ProductId);
+
+        builder.HasOne<Order>(oi => oi.Order)
+               .WithMany(o => o.OrderItems)
+               .HasForeignKey(oi => oi.OrderId);
+
+        builder.ToTable("OrderItems");
+    }
+}
